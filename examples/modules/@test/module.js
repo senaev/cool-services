afcc347c6d467c15851ff588fcac4bc16995dae5
Senaev Andrey <gsacryfice@gmail.com>
2015-11-10 20:26:10 +0500

    Переделал алгоритм работы, сервисы пока сломал

---
+++ examples/modules/@test/module.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var ServiceError;
+module.exports = {
+    init: function(resolve) {
+        ServiceError = this.ServiceError;
+        resolve();
+    },
+    methods: {
+        returnParamsWithAddValue: {
+            isPublic: true,
+            method: function(params) {
+                params.add = 'add';
+                return params;
+            }
+        },
+        throwError1: {
+            isPublic: true,
+            method: function() {
+                let hello = 'hello';
+                hello();
+            }
+        },
+        throwError2: {
+            isPublic: true,
+            method: function() {
+                throw 123;
+            }
+        },
+        throwError3: {
+            isPublic: true,
+            method: function() {
+                throw '123'
+            }
+        },
+        throwError4: {
+            isPublic: true,
+            method: function() {
+                throw new Error('123');
+            }
+        },
+        throwError5: {
+            isPublic: true,
+            method: function() {
+                throw new ServiceError('123');
+            }
+        }
+    },
+    helpers: {}
+};
